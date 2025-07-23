dcf10ec6d6e1a219e23179ba047cd840c74a7def
hayley-d <hayley@codehesion.com>
2025-07-23 14:51:39 +0200

    finish part 2

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -65,6 +65,6 @@ class DoctorsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def doctor_params
-      params.require(:doctor).permit(:name)
+      params.require(:doctor).permit(:name, :active)
     end
 end
