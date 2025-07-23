bcd2e83c5ab7a1d0215b05cafb14aeedfa299798
hayley-d <hayley@codehesion.com>
2025-07-23 15:21:15 +0200

    - update controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,25 +1,20 @@
 class AddressesController < ApplicationController
   before_action :set_address, only: %i[ show edit update destroy ]
 
-  # GET /addresses or /addresses.json
   def index
     @addresses = Address.all
   end
 
-  # GET /addresses/1 or /addresses/1.json
   def show
   end
 
-  # GET /addresses/new
   def new
     @address = Address.new
   end
 
-  # GET /addresses/1/edit
   def edit
   end
 
-  # POST /addresses or /addresses.json
   def create
     @address = Address.new(address_params)
 
@@ -34,7 +29,6 @@ class AddressesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /addresses/1 or /addresses/1.json
   def update
     respond_to do |format|
       if @address.update(address_params)
@@ -47,7 +41,6 @@ class AddressesController < ApplicationController
     end
   end
 
-  # DELETE /addresses/1 or /addresses/1.json
   def destroy
     @address.destroy!
 
@@ -58,12 +51,10 @@ class AddressesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_address
       @address = Address.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def address_params
       params.require(:address).permit(:street, :user_id)
     end
