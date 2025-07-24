dd014adf2f725f7edd5e21e1540e2ff26fa8b234
hayley-d <hayley@codehesion.com>
2025-07-24 07:28:38 +0200

    finish serializers

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,3 +1,11 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :client, :doctor, :date
+
+  def client
+    "#{object.user.name} #{object.user.last_name}"
+  end
+
+  def doctor
+    object.doctor.name
+  end
 end
--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,3 +1,7 @@
 class DoctorSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :available
+
+  def available
+    object.active
+  end
 end
